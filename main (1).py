ef077e5dadda154da656e5e387a8b90f3dee8d8d
Trible-A <144207706+Trible-A@users.noreply.github.com>
2023-09-26 13:19:39 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,14 +1,42 @@
-# Default function to implement conditions to check leap year  
-def CheckLeap(Year):  
-  # Checking if the given year is leap year  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  # Else it is not a leap year  
-  else:  
-    print ("Given Year is not a leap Year")  
-# Taking an input year from user  
-Year = int(input("Enter the number: "))  
-# Printing result  
-CheckLeap(Year)
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance=0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print(f"Deposited ${amount} into account {self.__account_number}")
+    else:
+      print("Invalid deposit amount. Please enter a positive amount.")
+
+  def withdraw(self, amount):
+    if 0 < amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print(f"Withdrew ${amount} from account {self.__account_number}")
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+    print(f"Account Number: {self.__account_number}")
+    print(f"Account Holder: {self.__account_holder_name}")
+    print(f"Account Balance: ${self.__account_balance}")
+
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+  # Create an instance of BankAccount
+  account1 = BankAccount("12345", "John Doe", 1000)
+
+  # Display initial balance
+  account1.display_balance()
+
+  # Make deposits and withdrawals
+  account1.deposit(500)
+  account1.withdraw(200)
+  account1.withdraw(1500)
+
+  # Display updated balance
+  account1.display_balance()
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# Python 3 program to find 
-# factorial of given number 
-def factorial(n):
-    if n < 0: 
-        return 0 
-    elif n == 0 or n == 1:
-         return 1 
-    else:
-        fact = 1
-        while(n > 1): 
-            fact *= n
-            n -= 1
-        return fact
-# Driver Code
-num = 5
-print("Factorial of", num, "is",
-factorial (num))
-# This code is contributed by Dharmi
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
